13440ccf453d02aad028fe78d9d9fd63bab94428
AapoApustaja <aapolarsen29@gmail.com>
2026-08-03 13:45:28 +0300

    Alustava kamera movement

--- Assets/Scripts/CameraMovemint.cs
+++ Assets/Scripts/CameraMovemint.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraMovemint : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
+    private Vector3 offset = new Vector3(0, 1.0f, -4f);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +14,12 @@ public class CameraMovemint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		transform.position = player.position + offset;
-	}
+        transform.position = player.position + player.rotation * offset;
+
+        Vector3 forward = player.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(forward, Vector3.up);
+
+        transform.rotation = targetRotation;
+    }
 }
