98b03398ee4f865c7cfd6e10df9ee99d7f00c848
OssiLeppanen <ossi.leppanen03@gmail.com>
2026-08-03 12:35:52 +0300

    pistimelle hitbox ja sinne koodi mihin voi osuu

--- Assets/Scripts/StingHit.cs
+++ Assets/Scripts/StingHit.cs
@@ -2,14 +2,35 @@ using UnityEngine;
 
 public class StingHit : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Tarkistetaan pistimen osumat (other antaa tiedot mihin törmätty)
+	private void OnTriggerEnter(Collider other)
+	{
+		// tehdään suckable muuttuja
+        Suckable IsSuckable = other.GetComponent<Suckable>();
+
+        // jos löytyy suckable componentti niin edetää
+        if (IsSuckable != null)
+        {
+            // haetaan hyttysen body ja jähmetetään
+            Rigidbody rb = GetComponentInParent<Rigidbody>();
+            rb.isKinematic = true;
+
+            // laitetaan hyttynen kiinni objektiin
+			transform.parent.SetParent(other.transform);
+
+		}
+
+
+	}
+	// Update is called once per frame
+	void Update()
     {
         
     }
--- Assets/Scripts/Suckable.cs
+++ Assets/Scripts/Suckable.cs
@@ -11,24 +11,6 @@ public class Suckable : MonoBehaviour
         
     }
 
-	private void OnCollisionEnter(Collision collision)
-	{
-        //Tunnistetaan ja otetaan hyty talteen sek‰ sen body
-		Hyty = collision.gameObject;
-        Rigidbody rb = Hyty.GetComponent<Rigidbody>();
-        if(rb != null )
-        {
-            // j‰hmett‰‰ hyty paikalleen
-			rb.isKinematic = true;
-
-		}
-		// liimataan hyttynen ja objekti yhteen parenting avulla
-		collision.transform.SetParent(transform);
-
-		//collision.gameObject.transform.SetParent(this.transform);
-
-	}
-
 	// Update is called once per frame
 	void Update()
     {
