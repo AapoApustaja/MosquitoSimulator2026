0eff58bf7b7ab793cb3bbb7948bb5974be524dca
OssiLeppanen <ossi.leppanen03@gmail.com>
2026-08-03 14:13:16 +0300

    jó

--- Assets/Scripts/CameraMovemint.cs
+++ Assets/Scripts/CameraMovemint.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class CameraMovemint : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
-    //public Vector3 euleerioffset;
+    private Vector3 offset = new Vector3(0, 1.0f, -4f);
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +16,19 @@ public class CameraMovemint : MonoBehaviour
     // Update is called once per frame  
     void Update()
     {
-		transform.position = player.position + offset;
-	}
+
+        transform.position = player.position + player.rotation * offset;
+
+        Vector3 forward = player.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(forward, Vector3.up);
+
+        transform.rotation = targetRotation;
+    }
+
+
+
+        
+	
+
 }
