57b5d7e90b41cd2fe04c77dd59f3ef08d7f4d939
OssiLeppanen <ossi.leppanen03@gmail.com>
2026-08-04 09:18:05 +0300

    ulostus kiinni jäämisestä

--- Assets/Scripts/StingHit.cs
+++ Assets/Scripts/StingHit.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 
 public class StingHit : MonoBehaviour
 {
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    bool stuck = false;
+	Rigidbody rb;
+	Vector3 direction;
+	float cooldown = 2f;
+	float cooldownTimer = 0f;
+
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
-        
-    }
+		rb = GetComponentInParent<Rigidbody>();
+	}
 
     // Tarkistetaan pistimen osumat (other antaa tiedot mihin tˆrm‰tty)
 	private void OnTriggerEnter(Collider other)
@@ -18,12 +25,17 @@ public class StingHit : MonoBehaviour
         // jos lˆytyy suckable componentti niin edet‰‰
         if (IsSuckable != null)
         {
-            // haetaan hyttysen body ja j‰hmetet‰‰n
-            Rigidbody rb = GetComponentInParent<Rigidbody>();
-            rb.isKinematic = true;
+			stuck = true;
+			// haetaan hyttysen body ja j‰hmetet‰‰n
+	
+			rb.isKinematic = true;
 
-            // laitetaan hyttynen kiinni objektiin
+			// Tehd‰‰n hytyst‰ toisen objektin lapsi
 			transform.parent.SetParent(other.transform);
+
+			//Lasketaan hytyn ja objektin v‰linen et‰isyys
+			direction = transform.parent.position - other.transform.position;
+		
 
 		}
 
@@ -32,6 +44,32 @@ public class StingHit : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        
-    }
+
+
+		if (stuck)
+		{
+			//Aloteitaan suki suki
+			if (Input.GetKey(KeyCode.Space))
+			{
+				Console.WriteLine("moi");
+			}
+
+
+			// l‰hdet‰‰n irti
+			else if (Input.GetKey(KeyCode.LeftShift))
+			{
+				stuck = false;
+				//Siirret‰‰n hyty pois sis‰lt‰
+				transform.parent.position += direction * 0.1f;
+				//Poistetaan hytyn parent
+				transform.parent.SetParent(null);
+				rb.isKinematic = false;
+
+
+
+
+			}
+		}
+		
+	}
 }
