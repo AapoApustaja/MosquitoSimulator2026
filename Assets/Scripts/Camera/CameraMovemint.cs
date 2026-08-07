6ea8a9dadaeb913148d00939261b02e0b0e9aaf0
AapoApustaja <aapolarsen29@gmail.com>
2026-08-07 17:25:07 +0300

    kameraa vähä paranneltu ja kommentoitu

--- Assets/Scripts/Camera/CameraMovemint.cs
+++ Assets/Scripts/Camera/CameraMovemint.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class CameraMovemint : MonoBehaviour
 {
     public Transform player;
+
     private Vector3 offset = new Vector3(0, 1.0f, -4f);
 
+    /// <summary>
+    /// Mitkä objektit törmää kameraan (tähän default aina)
+    /// </summary>
     [SerializeField] private LayerMask collisionMask;
 
-    [SerializeField] private float smoothTime = 0.15f;
-    [SerializeField] private float rotationSpeed = 8f;
+    /// <summary>
+    /// Miten kauan kameralla kestää smoothata
+    /// </summary>
+    [SerializeField] private float smoothTime = 0.30f;
+
+    /// <summary>
+    /// Miten nopeesti kamera kääntyy rotationin mukana
+    /// </summary>
+    [SerializeField] private float rotationSpeed = 25f;
 
     private Vector3 velocity;
 
@@ -24,6 +35,7 @@ public class CameraMovemint : MonoBehaviour
 
     }
 
+    // Kamerat lateupdateen niin ei lagaa
     private void LateUpdate()
     {
         if (ControlsMenu.MovementType == 1)
@@ -37,27 +49,44 @@ public class CameraMovemint : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Normi kontrolleille kamera
+    /// </summary>
     private void standardCamera()
     {
+        // Haluttu kameran positio
         Vector3 desiredPosition = player.position + player.rotation * offset;
+
+        // Alkuperänen kameran positio olettaa että ei ole mitään tiellä
         Vector3 targetPosition = desiredPosition;
 
+
         Vector3 dir = desiredPosition - player.position;
+
         float distance = dir.magnitude;
+
         dir.Normalize();
 
+        // Kattoo onko halutun kamerapositionin ja pelaajan välillä mitää estettä
         if (Physics.SphereCast(player.position, 0.3f, dir, out RaycastHit hit, distance, collisionMask))
         {
+            // Liikutetaan kameraa esteen eteen
             targetPosition = hit.point - dir * 0.3f;
         }
 
+        // Smoothisti liikuttaa kameran haluttuun paikkaan
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
+        // Laskee rotaation joka liikuttaa kameran pelaajaa kohti 
         Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
 
+        // Liikutta kameran rotaation
         transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,rotationSpeed * Time.deltaTime);
     }
 
+    /// <summary>
+    /// Helikopterille kamera
+    /// </summary>
     private void helicopterCamera()
     {
         transform.position = player.position + player.rotation * offset;
