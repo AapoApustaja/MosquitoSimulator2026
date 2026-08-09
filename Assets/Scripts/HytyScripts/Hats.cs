48fbed92311971c4427be9f1d190c0599c11f239
AapoApustaja <aapolarsen29@gmail.com>
2026-08-09 05:58:56 +0300

    Added hat support, hat loading, cat ears and halo hat

---
+++ Assets/Scripts/HytyScripts/Hats.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Hats : MonoBehaviour
+{
+
+    [SerializeField] private Transform hatParent;
+
+    private GameObject currentHat;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LoadHat("Halo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void LoadHat(string hatName)
+    {
+        // Poista nykyne
+        if (currentHat != null)
+        {
+            Destroy(currentHat);
+        }
+
+        // lata hatun modeli
+        GameObject hatPrefab = Resources.Load<GameObject>("Hats/" + hatName);
+
+        // hatun luonti
+        currentHat = Instantiate(hatPrefab, hatParent, false);
+    }
+}
