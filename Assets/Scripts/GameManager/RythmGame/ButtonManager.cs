63f3dd167f2165c4ff83d991878a7d2365934d94
OssiLeppanen <ossi.leppanen03@gmail.com>
2026-08-12 12:18:47 +0300

    rytmipeliä aloteltu

---
+++ Assets/Scripts/GameManager/RythmGame/ButtonManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Button buttonLeft;
+    public Button buttonUp;
+    public Button buttonDown;
+    public Button buttonRight;
+
+
+
+	public KeyCode keyleft = KeyCode.Alpha1;
+	public KeyCode keyup = KeyCode.Alpha2;
+	public KeyCode keydown = KeyCode.Alpha3;
+	public KeyCode keyright = KeyCode.Alpha4;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+    {	
+		if(Input.GetKeyDown(keyleft))
+		{
+			SelectButton(buttonLeft);
+		}
+		if (Input.GetKeyDown(keyup))
+		{
+			SelectButton(buttonUp);
+		}
+		if (Input.GetKeyDown(keydown))
+		{
+			SelectButton(buttonDown);
+		}
+		if (Input.GetKeyDown(keyright))
+		{
+			SelectButton(buttonRight);
+		}
+
+
+	}
+
+	void SelectButton(Button button)
+	{
+		EventSystem.current.SetSelectedGameObject(button.gameObject);
+		Debug.Log("Selected: " + button.name);
+	}
+
+}
